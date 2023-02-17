14c251a199e74763e4fda7d349d338341f0e55af
EduardSekan <sekaneduard@gmail.com>
2023-02-17 19:08:30 +0200

    remove code

--- index.js
+++ index.js
@@ -35,19 +35,3 @@ program.parse(process.argv)
 const argv = program.opts()
 
 invokeAction(argv)
-
-// ----------------------------------------------------------------------
-
-// # Получаем и выводим весь список контактов в виде таблицы (console.table)
-// node index.js --action list
-
-// # Получаем контакт по id
-// node index.js --action get --id 5
-
-// # Добавялем контакт
-// node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
-
-// # Удаляем контакт
-// node index.js --action remove --id=3
-
-// ----------------------------------------------------------------------
