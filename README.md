681a5a1be7cdb1583ddd74c76ad52df29e9952d0
EduardSekan <sekaneduard@gmail.com>
2023-02-17 19:07:27 +0200

    add all code

---
+++ README.md
@@ -0,0 +1,13 @@
+# HW-1
+
+# Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+https://monosnap.com/file/3m5DpPu5DxOKXWRw4k0L36WhDmTCUH
+
+# Отримуємо контакт по id
+https://monosnap.com/file/zKjtW6VtYwku42DFmpjYEAtfIMrUcs
+
+# Додаємо контакт
+https://monosnap.com/file/lw5f1XhFtlcDZtUYS6x7tuz9MAlMYW
+
+# Видаляємо контакт
+https://monosnap.com/file/WjaFr1pbllfeZH6yHK1tCQsLfhXO0l
--- index.js
+++ index.js
@@ -1,2 +1,53 @@
+const { Command } = require("commander")
+const program = new Command()
 
-console.log('NODA');
+const contacts = require("./db/contacts")
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+    switch (action) {
+        case "list":
+            const allContacts = await contacts.listContacts()
+            return console.table(allContacts)
+        case "get":
+            const oneContact = await contacts.getContactById(id)
+            return console.table(oneContact)
+        case "add":
+            const newContact = await contacts.addContact({ name, email, phone })
+            return console.table(newContact)
+        case "remove":
+            const deleteBook = await contacts.removeContact(id)
+            return console.table(deleteBook)
+
+        default:
+            console.warn("\x1B[31m Unknown action type!")
+    }
+}
+
+program
+    .option("-a, --action <type>", "choose action")
+    .option("-i, --id <type>", "user id")
+    .option("-n, --name <type>", "user name")
+    .option("-e, --email <type>", "user email")
+    .option("-p, --phone <type>", "user phone")
+
+program.parse(process.argv)
+
+const argv = program.opts()
+
+invokeAction(argv)
+
+// ----------------------------------------------------------------------
+
+// # Получаем и выводим весь список контактов в виде таблицы (console.table)
+// node index.js --action list
+
+// # Получаем контакт по id
+// node index.js --action get --id 5
+
+// # Добавялем контакт
+// node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+
+// # Удаляем контакт
+// node index.js --action remove --id=3
+
+// ----------------------------------------------------------------------
